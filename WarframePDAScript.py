ab8d49cd2feb7fed9cd78bed2bc21bdc056a612e
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2021-02-08 18:29:42 -0800

    saving my progress

---
+++ WarframePDAScript.py
@@ -0,0 +1,145 @@
+import requests
+from requests.auth import HTTPBasicAuth 
+from bs4 import BeautifulSoup
+import base64
+import json
+import time
+
+
+
+items = [
+    'ash', 'atlas', 'banshee', 'chroma', 'ember', 
+    'equinox', 'frost', 'hydroid', 'ivara', 'limbo', 
+    'loki', 'mag', 'mesa', 'mirage', 'nekros', 
+    'nezha', 'nova', 'nyx', 'oberon', 'rhino', 
+    'saryn', 'titania', 'trinity', 'valkyr', 'vauban', 
+    'volt', 'wukong', 'zephyr'
+]
+
+items.sort()
+
+
+
+def get_token():
+    client_id = "client-id"
+    client_secret = "client-secret"
+
+    unencoded_key = client_id + ":" + client_secret
+
+#     print(unencoded_key)
+
+    urlSafeEncodedBytes = base64.urlsafe_b64encode(unencoded_key.encode("utf-8"))
+    encoded_key = str(urlSafeEncodedBytes, "utf-8")
+
+#     print(encoded_key)
+
+    # "grant_type=password&username=" + username + "&password=" + password
+
+
+    username = "testuser"
+    password = "password"
+
+
+    url = "https://warframepda.herokuapp.com/login"
+    userAuthData = "grant_type=password&username=" + username + "&password=" + password
+    headers = {'Authorization': 'Basic ' + encoded_key,
+              'Content-Type': 'application/x-www-form-urlencoded'
+              }
+    # payload = {'name':'testtim', 'password': 'testpass'}
+
+    response = requests.post(url, userAuthData, headers=headers)
+
+    print(response.json())
+    return response.json()['access_token']
+
+
+
+def request_data(url):
+    headers = {'Content-Type': 'text/html',}
+    response = requests.get(url, headers=headers)
+
+    # since the site from which this data is scraped has a very dynamic 
+    # method for displaying orders, it's easier to simply sort through
+    # the raw page source than it is to attempt to sort by xpath or 
+    # similar method
+
+    # this section sorts through the raw page and pulls what becomes
+    # a json object containing all orders for the given item
+    page_soup = BeautifulSoup(response.text, 'html.parser')
+    page_rough = page_soup.find_all(id = 'application-state')
+
+    page_rough = str(page_rough)
+    page_rough_len = len(page_rough)
+    page_json = page_rough[56:page_rough_len - 10]
+
+    page_json_loaded = json.loads(page_json)
+
+
+
+    # parses for sell values
+    page_sell_values = []
+
+    for page_line in page_json_loaded['payload']['orders']:
+
+        # checks for users that are set to 'online/ingame', are in the 'en/international' region, and are placing 'sell' orders
+        if page_line['user']['status'] == 'ingame' and page_line['user']['region'] == 'en' and page_line['order_type'] == 'sell':
+
+            # pulls out the platinum cost and seller name of each order
+            price = int(page_line['platinum'])
+            name = str(page_line['user']['ingame_name'])
+
+            # puts the sellername and price into the correct format for the backend, then adds them to a list
+            page_sell_values.append({"seller" : {"sellername" : name}, "price": price})
+
+
+    temp_orders_list = sorted(page_sell_values, key=lambda k: k['price'])
+
+    orders_list = []
+
+    for order in temp_orders_list:
+        orders_list.append(order)
+        if len(orders_list) >= 5:
+            break
+
+#     print(orders_list)
+    return orders_list
+
+token = get_token()
+
+item_part_names = ["neuroptics", "set", "systems", "chassis", "blueprint"]
+item_part_names = sorted(item_part_names)
+
+for item in items: # each warframe is an "item"
+    
+    constructed_parts_list = []
+    for part in item_part_names: # makes a request for each 'part' of the item
+
+        created_link = 'https://warframe.market/items/' + item + '_prime_' + part
+        
+        returned_data = request_data(created_link) # makes the request to WarframeMarket using the request_data function
+        
+        # appends all 5 parts to the 'constructed_parts_list' (one per loop iteration)
+        constructed_parts_list.append({
+            "partname": part.capitalize(),
+            "orders": returned_data
+        })
+        print("fetched data for " + item + " prime " + part)
+        time.sleep(1)
+        
+        
+    constructed_item = {
+        "itemname": item.capitalize(),
+        "imageurl": "www.testurl.com",
+        "parts": constructed_parts_list
+    }
+    
+    print(constructed_item)
+    
+    post_url = "https://warframepda.herokuapp.com/items/item"
+    headers = {'Authorization': 'Bearer ' + token,
+              'Content-Type': 'application/JSON'
+              }
+    response = requests.post(post_url, headers=headers, json=constructed_item)
+    
+    print(response)
+    
